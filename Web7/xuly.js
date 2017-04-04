e2863bffc9d3d3019ea8d9f9ab01b5d4107d9944
hientq6724 <hientq6724@co-well.com.vn>
2017-04-04 09:31:53 +0700

    add animate scroll and affix menu

--- Web7/xuly.js
+++ Web7/xuly.js
@@ -3,4 +3,27 @@ $(function(){
 	$(window).resize(function(){
 		$('.over-img').css('line-height',$('.sanpham').height()+'px');
 	});
+	$(window).scroll(function(){
+		var curent_position = $('body').scrollTop();
+
+		//console.log($('body').scrollTop());
+
+		if (curent_position > 337) {
+			$('.navbar-fixed-top').addClass('thunho');
+		}else if(curent_position < 110){
+			$('.navbar-fixed-top').removeClass('thunho');
+		}
+
+		if (curent_position > 300) {
+			$('.top2').addClass('top2-animate');
+		} else {
+			$('.top2').removeClass('top2-animate');
+		}
+	});
+
+	$('.menu li a').on('click', function(){
+		$('body').animate({scrollTop : $($(this).attr('href')).offset().top -100}, 2000, "easeOutBack");
+		return false;
+		//alert($(this).attr('href'));
+	});
 });
